c46565c7aec1b8c2829ef580277b035e586a5617
abhishek <abhishek.dongle@robosoftin.com>
2025-07-30 09:13:13 +0530

    feat: add initial setup for data module

---
+++ data/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.data"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "DummyApp"
 include(":app")
- 
+include(":data")
